58c8d6d307a038a16e06a00122845324c591072f
rileydollar8@gmail.com <ryan.anzaye@student.moringaschool.com>
2024-04-04 18:18:48 +0300

    task_lister

--- src/index.js
+++ src/index.js
@@ -1,3 +1,19 @@
+
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
-});
+  const form = document.getElementById("create-task-form")
+  const taskList = document.getElementById("tasks")
+
+  form.addEventListener("submit", (event)=> {
+    event.preventDefault()
+    
+    const taskInput = document.getElementById("new-task-description");
+    const taskText = taskInput.value;
+
+    if (taskText) {
+      const newTask = document.createElement("li");
+      newTask.textContent = taskText
+      taskList.appendChild(newTask)
+      taskInput.value = "" 
+    }
+   } )
+})
